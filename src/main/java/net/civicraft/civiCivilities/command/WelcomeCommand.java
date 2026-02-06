dbd5457c4c1fabb2374c80cc8ddfa2eeb9bcedfe
sspeedwagonn <burnerforcarter@gmail.com>
2026-02-05 23:28:43 -0800

    Command skeleton

--- src/main/java/net/civicraft/civiCivilities/command/WelcomeCommand.java
+++ src/main/java/net/civicraft/civiCivilities/command/WelcomeCommand.java
@@ -1,6 +1,15 @@
 package net.civicraft.civiCivilities.command;
 
-public class WelcomeCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class WelcomeCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
+        return false;
+    }
     /*
     Player runs /welcome or /wb or another custom command which welcomes all players who joined in the past 30 seconds
      */
